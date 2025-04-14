d6c56a1881310d605fe5bb1a601f6837a32cd148
cronzevid <cronzevid@gmail.com>
2025-04-15 00:02:40 +0300

    bump deprecations

--- main.go
+++ main.go
@@ -1,168 +1,166 @@
 package main
 
 import (
-  "log"
-  "os"
-  "time"
-  "math/rand"
-  "flag"
-  "sync"
+	"flag"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
 
-  "crypto/x509"
-  "crypto/tls"
-  "io/ioutil"
+	"crypto/tls"
+	"crypto/x509"
 
-  "github.com/streadway/amqp"
+	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
-  if err != nil {
-    log.Fatalf("%s: %s", msg, err)
-    os.Exit(1)
-  }
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		os.Exit(1)
+	}
 }
 
 func genRandStr() string {
-  charset := "abcdefghijklmnopqrstuvwxyz"
+	charset := "abcdefghijklmnopqrstuvwxyz"
 
-  var seededRand *rand.Rand = rand.New(
-    rand.NewSource(time.Now().UnixNano()))
+	var seededRand *rand.Rand = rand.New(
+		rand.NewSource(time.Now().UnixNano()))
 
-  b := make([]byte, 10)
-  for i := range b {
-    b[i] = charset[seededRand.Intn(len(charset))]
-  }
+	b := make([]byte, 10)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
 
-  return string(b)
+	return string(b)
 }
 
 func declareQueue(ch *amqp.Channel, queueArg *string) amqp.Queue {
-  queue := *queueArg
-  q, err := ch.QueueDeclare(
-    queue, // name
-    false,   // durable
-    false,   // delete when unused
-    false,   // exclusive
-    false,   // no-wait
-    nil,     // arguments
-  )
-  failOnError(err, "Failed to declare a queue")
+	queue := *queueArg
+	q, err := ch.QueueDeclare(
+		queue, // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
 
-  return q
+	return q
 }
 
 func sendToQueue(ch *amqp.Channel, q amqp.Queue, msgArg *string, infArg *bool) {
-  body := *msgArg
-  infinite := *infArg
+	body := *msgArg
+	infinite := *infArg
 
-  if body == "rand" {
-   body = genRandStr()
-  }
+	if body == "rand" {
+		body = genRandStr()
+	}
 
-  var wg sync.WaitGroup
-  wg.Add(1)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-  go func() {
-    defer wg.Done()
-    for {
-      err := ch.Publish(
-        "",     // exchange
-        q.Name, // routing key
-        false,  // mandatory
-        false,  // immediate
-        amqp.Publishing {
-          ContentType: "text/plain",
-          Body:        []byte(body),
-        })
-      failOnError(err, "Failed to publish a message")
+	go func() {
+		defer wg.Done()
+		for {
+			err := ch.Publish(
+				"",     // exchange
+				q.Name, // routing key
+				false,  // mandatory
+				false,  // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				})
+			failOnError(err, "Failed to publish a message")
 
-      if infinite != true {
-        break
-      } else {
-        log.Printf("Sent a message: %s", body)
-        time.Sleep(1000 * time.Millisecond)
-      }
-    }
-  }()
+			if infinite != true {
+				break
+			} else {
+				log.Printf("Sent a message: %s", body)
+				time.Sleep(1000 * time.Millisecond)
+			}
+		}
+	}()
 
-  wg.Wait()
+	wg.Wait()
 }
 
 func receiveFromQueue(ch *amqp.Channel, q amqp.Queue) {
-  msgs, err := ch.Consume(
-    q.Name, // queue
-    "",     // consumer
-    true,   // auto-ack
-    false,  // exclusive
-    false,  // no-local
-    false,  // no-wait
-    nil,    // args
-  )
-  failOnError(err, "Failed to register a consumer")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
 
-  forever := make(chan bool)
+	forever := make(chan bool)
 
-  go func() {
-    for d := range msgs {
-      log.Printf("Received a message: %s", d.Body)
-    }
-  }()
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
 
-  log.Printf(" [*] Waiting for messages on %s. To exit press CTRL+C", q.Name)
+	log.Printf(" [*] Waiting for messages on %s. To exit press CTRL+C", q.Name)
 
-  <-forever
+	<-forever
 }
 
-
 func main() {
-  actionArg := flag.String("act", "both", "send/recieve/both")
-  hostArg := flag.String("hst", "localhost:5672", "host:port")
-  authArg := flag.String("auth", "guest:guest", "login:pass")
-  queueArg := flag.String("q", "default", "name of queue to push to")
-  msgArg := flag.String("msg", "knock_knock", "message to push to queue")
-  infArg := flag.Bool("inf", false, "send msg infinitely")
-  tlsArg := flag.Bool("tls", false, "port is TLS")
+	actionArg := flag.String("act", "both", "send/recieve/both")
+	hostArg := flag.String("hst", "localhost:5672", "host:port")
+	authArg := flag.String("auth", "guest:guest", "login:pass")
+	queueArg := flag.String("q", "default", "name of queue to push to")
+	msgArg := flag.String("msg", "knock_knock", "message to push to queue")
+	infArg := flag.Bool("inf", false, "send msg infinitely")
+	tlsArg := flag.Bool("tls", false, "port is TLS")
 
-  flag.Parse()
+	flag.Parse()
 
-  var conn *amqp.Connection
-  var err error
+	var conn *amqp.Connection
+	var err error
 
-  if *tlsArg {
-    //cfg := new(tls.Config)
-    cfg := &tls.Config{InsecureSkipVerify: true}
+	if *tlsArg {
+		//cfg := new(tls.Config)
+		cfg := &tls.Config{InsecureSkipVerify: true}
 
-    cfg.RootCAs = x509.NewCertPool()
+		cfg.RootCAs = x509.NewCertPool()
 
-    if ca, err := ioutil.ReadFile("ssl/ca.pem"); err == nil {
-        cfg.RootCAs.AppendCertsFromPEM(ca)
-    }
+		if ca, err := os.ReadFile("ssl/ca.pem"); err == nil {
+			cfg.RootCAs.AppendCertsFromPEM(ca)
+		}
 
-    if cert, err := tls.LoadX509KeyPair("ssl/cert.pem", "ssl/key.pem"); err == nil {
-        cfg.Certificates = append(cfg.Certificates, cert)
-    }
+		if cert, err := tls.LoadX509KeyPair("ssl/cert.pem", "ssl/key.pem"); err == nil {
+			cfg.Certificates = append(cfg.Certificates, cert)
+		}
 
-    conn, err = amqp.DialTLS("amqps://" + *authArg + "@" + *hostArg, cfg)
-    failOnError(err, "Failed to connect over TLS to RabbitMQ")
-  } else {
-    conn, err = amqp.Dial("amqp://" + *authArg + "@" + *hostArg)
-    failOnError(err, "Failed to connect to RabbitMQ")
-  }
+		conn, err = amqp.DialTLS("amqps://"+*authArg+"@"+*hostArg, cfg)
+		failOnError(err, "Failed to connect over TLS to RabbitMQ")
+	} else {
+		conn, err = amqp.Dial("amqp://" + *authArg + "@" + *hostArg)
+		failOnError(err, "Failed to connect to RabbitMQ")
+	}
 
-  defer conn.Close()
+	defer conn.Close()
 
-  ch, err := conn.Channel()
-  failOnError(err, "Failed to open a channel")
-  defer ch.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-  queue := declareQueue(ch, queueArg)
+	queue := declareQueue(ch, queueArg)
 
-  if *actionArg == "both" {
-    sendToQueue(ch, queue, msgArg, infArg)
-    receiveFromQueue(ch, queue)
-  } else if *actionArg == "receive" {
-    receiveFromQueue(ch, queue)
-  } else if *actionArg == "send" {
-    sendToQueue(ch, queue, msgArg, infArg)
-  }
+	if *actionArg == "both" {
+		sendToQueue(ch, queue, msgArg, infArg)
+		receiveFromQueue(ch, queue)
+	} else if *actionArg == "receive" {
+		receiveFromQueue(ch, queue)
+	} else if *actionArg == "send" {
+		sendToQueue(ch, queue, msgArg, infArg)
+	}
 }
